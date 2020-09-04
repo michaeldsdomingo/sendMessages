52ce8dbcc4301d67544ea20521c9d16d986a785d
michaeldsdomingo <michael.ds.domingo@gmail.com>
2020-09-04 15:41:59 -0700

    finished filtering logic, displaying results, and error checking

--- src/SendTags.js
+++ src/SendTags.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import axios from 'axios';
 
 export default function SendTags () {
     const [recipients, updateRecipients] = useState("")
@@ -6,6 +7,73 @@ export default function SendTags () {
     const [sendTo, updateSendTo] = useState("")
     const [sendType, updateSendType] = useState("")
     const [sent, updateSent] = useState(false)
+
+    const getContacts = () => {
+        return axios.get('https://sheetdb.io/api/v1/aka2sv6jd00dh')
+    }
+
+    const onGetContactsSuccess = (response) => {
+        const sendToArray = sendTo.split(",")
+        let results;
+        let resultsDescription;
+        const sendTypeParsed = parseSendType(sendType);
+
+        if (sendTypeParsed !== 'error') {
+            results = response.data.filter((contact) => {
+                let isMatch;
+                if (qualifier.toLowerCase() === "and") {
+                    isMatch = true;
+                    sendToArray.forEach(sendTo => {
+                        if (!contact[sendTypeParsed].includes(sendTo)) {
+                            isMatch = false;
+                        }
+                    })
+                } else if (qualifier.toLowerCase() === "or") {
+                    isMatch = false;
+                    sendToArray.forEach(sendTo => {
+                        if (contact[sendTypeParsed].includes(sendTo)) {
+                            isMatch = true;
+                        }
+                    })
+                }
+                return isMatch
+            })
+            console.log(results)
+            resultsDescription = results.length > 0 ? results.map((result) => {
+                return <div key={result.id}>{result.firstName} {result.lastName} from {result.organizationId}</div>
+            }) : 'No records found'
+        }
+        else {
+            resultsDescription = 'Invalid SendType'
+        }
+       
+        updateRecipients(resultsDescription)
+        updateSent(true)
+    }
+
+    const onGetContactsError = (error) => {
+        updateRecipients('Server Error')
+        updateSent(true)
+    }
+
+    const parseSendType = (sendType) => {
+        console.log(sendType.toLowerCase())
+        if (sendType.toLowerCase() === 'tags') {
+            return 'tags'
+        }
+        else if (sendType.toLowerCase() === 'organization') {
+            return 'organization'
+        }
+        else if (sendType.toLowerCase().includes('first')) {
+            return 'firstName'
+        }
+        else if (sendType.toLowerCase().includes('last')) {
+            return 'lastName'
+        }
+        else {
+            return 'error'
+        }
+    }
 
     const handleChange = (event) => {
         const value = event.target.value
@@ -29,6 +97,7 @@ export default function SendTags () {
         /*  implement me
             hint: we will probably need to update state here to render the right parts
         */
+        getContacts().then(onGetContactsSuccess).catch(onGetContactsError)
     }
 
     return (
